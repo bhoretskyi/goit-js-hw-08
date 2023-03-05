e5c3a689c1d7402757fa53b6964f7408e7f5c610
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-03-05 17:55:46 +0100

    end 1st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const imgContainer = document.querySelector(".gallery");
+
+const imgElements = galleryItems.reduce(
+  (acum, item) =>
+    (acum += `<div class="gallery__item"> <a class="gallery__item" href="${item.original}"><img class="gallery__image"
+      src = "${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"/></a></div>`),
+  ""
+);
+
+imgContainer.insertAdjacentHTML("beforeend", imgElements);
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+ 
+});
